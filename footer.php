692db8e410d202ce27cfe150bc262de3517182b7
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-04-21 14:40:05 +0200

    Wip

--- footer.php
+++ footer.php
@@ -84,17 +84,7 @@
                         <?php include get_template_directory() . '/img/icons/instagram.php'; ?>
                         </a>
                     <?php endif; ?>
-                    <?php 
-                    $link = get_field('twitter', 'option');
-                    if( $link ): 
-                        $link_url = $link['url'];
-                        $link_title = $link['title'];
-                        $link_target = $link['target'] ? $link['target'] : '_self';
-                        ?>
-                        <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
-                        <?php include get_template_directory() . '/img/icons/twitter.php'; ?>
-                        </a>
-                    <?php endif; ?>
+                 
                     <?php 
                     $link = get_field('tripadvisor', 'option');
                     if( $link ): 
