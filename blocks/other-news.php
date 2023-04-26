a86aa5f92b87b281589f65e802773df484c3c0d6
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-04-26 16:35:01 +0200

    wip

--- blocks/other-news.php
+++ blocks/other-news.php
@@ -6,7 +6,7 @@
                     'post_type' => 'news',
                     'posts_per_page' => -1,
                     'orderby' => 'date',
-                    'order' => 'ASC'
+                    'order' => 'DESC'
                 )
                 );
                 ?>
@@ -14,9 +14,10 @@
                 <div class="col-span-3 sm:col-span-2 md:col-span-1">
                     <a href="<?php the_permalink(); ?>">
                         <img class="w-full h-auto aspect-video object-cover mb-1" src="<?php echo get_the_post_thumbnail_url(); ?>">
+                        <p class="text-gray-500 mt-1"><?php the_date(); ?></p>
                         <h2 class="text-25 leading-35  font-medium md:font-normal text-darkblue"><?php the_title();?></h2>
                         <p class="text-green mt-1"><?php if(ICL_LANGUAGE_CODE=='en'): ?>Read more<?php elseif(ICL_LANGUAGE_CODE=='nl'): ?>Lees meer<?php elseif(ICL_LANGUAGE_CODE=='pt'): ?>consulte Mais informação<?php endif; ?></p>
-                    </a>
+                        </a>
                 </div>
            <?php endwhile; wp_reset_query(); ?>
     </div>
