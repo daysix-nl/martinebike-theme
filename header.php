3b98cd100d87dd31df753e1b95eb709d861270ca
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-16 09:50:17 +0200

    GA

--- header.php
+++ header.php
@@ -10,6 +10,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://load.sst.martinsebike.com/estknhsm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2ZGG7K');</script>
+<!-- End Google Tag Manager -->
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -19,9 +26,15 @@
     />
     <title><?php bloginfo( 'name' ); ?> | <?php the_title(); ?></title>    
     <?php wp_head(); ?>
+
+
 </head>
 
 <body <?php body_class( 'page-field ' ); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://load.sst.martinsebike.com/ns.html?id=GTM-P2ZGG7K"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <header id="header-desktop" class="fixed top-0 z-[5] left-0 right-0 w-screen">
         <section class="mx-2 md:px-3 grid grid-cols-3 py-1">
             <div class="col-span-1 flex justify-start items-center">
