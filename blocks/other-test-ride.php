b290500f81b6484936955c21fdf3a8690f55a844
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-08-22 15:36:36 +0200

    wip

--- blocks/other-test-ride.php
+++ blocks/other-test-ride.php
@@ -1,4 +1,5 @@
 <!-- OTHER TEST RIDE -->
+<?php $currentUrl = $_SERVER['REQUEST_URI']; ?>
 <section class="mobile:h-[80vh] h-[30vh] md:h-[55vh] w-screen relative overflow-hidden bg-white">
     <img class="w-full h-full object-cover absolute top-0 right-0  image-full" src="https://martinsebike.com/wp-content/uploads/2023/02/MARTINS-E-BIKE-9194-scaled.jpg" alt="">
     <div class="container flex flex-col justify-center gap-3 z-2 h-full relative z-[2]">
